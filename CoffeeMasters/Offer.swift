1b33641c30f57c0e711f9b774f7717ccee783ac4
sganas360 <sganas@caesars.com>
2022-09-13 14:33:43 -0400

    added offers

--- CoffeeMasters/Offer.swift
+++ CoffeeMasters/Offer.swift
@@ -8,18 +8,29 @@
 import SwiftUI
 
 struct Offer: View {
-    var title = ""
-    var description = ""
-    
+    var title: String = ""
+    var description: String = ""
     
     var body: some View {
-        VStack{
-            Text(title)
-                .padding()
-                .font(.title)
-            Text(description)
-                .padding()
-        }
+        ZStack {
+            Image("BackgroundPattern")
+                .frame(maxWidth: .infinity, maxHeight: 300)
+                .clipped()
+            VStack(alignment: .center, spacing: 50) {
+                VStack {
+                    Text(title)
+                        .padding()
+                        .font(.title)
+                        .background(Color("CardBackground"))
+                        .padding(.bottom, 20)
+                    Text(description)
+                        .padding()
+                        .background(Color("CardBackground"))
+                }
+                .frame(maxWidth: .infinity, minHeight: 150, maxHeight: 150, alignment: .center)
+            }
+        }.background(Color.white)
+        
     }
 }
 
